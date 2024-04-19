9d9bb31e86bfb222fb2531eeb71016c7c588ee92
Nelson-Aguilar <157563219+Nelson-Aguilar@users.noreply.github.com>
2024-04-18 22:34:12 -0600

    Delete js directory

--- js/mensaje-enviado.js
+++
@@ -1,8 +0,0 @@
-
-export function mensajeEnviado(){
-    document.querySelector(".cartel-enviado").classList.add("mensaje-enviado");
-
-    setTimeout(()=>{
-        document.querySelector(".cartel-enviado").classList.remove("mensaje-enviado");
-    },3000);
-};

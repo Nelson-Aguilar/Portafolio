5b6ffb16c345ac83177f2579f992cf7fcc73c3ee
Nelson-Aguilar <157563219+Nelson-Aguilar@users.noreply.github.com>
2024-04-18 23:34:24 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,6 +26,16 @@ document.querySelector(".enviar").addEventListener("click", (event) =>{
 /*escucha el formulario. Si hace submit, hace un preventdefaul para que no refresque la pantalla. Valida las entradas e imprime mensaje de "enviado"*/
 
 document.querySelector("form").addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    fetch("https://formsubmit.co/alex.aguilar.sv@outlook.es", {method : "POST",
+    body: new FormData(event.target)
+    }).then((respuesta) => {
         document.querySelector("form").reset();
         mensajeEnviado();
+    }).catch((error) => alert(error));
+    
+
 });
+
